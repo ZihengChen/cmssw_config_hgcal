a0eb617abce75fbed6972b18ab886be888bf5145
Ziheng Chen <zava153@live.rhul.ac.uk>
2019-06-27 18:03:13 +0200

    Update crab3.py

--- crab3.py
+++ crab3.py
@@ -1,26 +1,26 @@
 from CRABClient.UserUtilities import config, getUsernameFromSiteDB
 config = config()
 
-config.General.requestName = 'muons_step2'
+config.General.requestName = 'muons_step3'
 config.General.workArea = 'crab_projects'
 config.General.transferOutputs = True
 config.General.transferLogs = False
 
 config.JobType.pluginName = 'Analysis'
-config.JobType.psetName = 'step2.py'
+config.JobType.psetName = 'step3.py'
 
 
-config.Data.inputDataset = '/muons/zichen-muons_step1-9f608cf1249bf52d0f768ac63af234d0/USER'
+config.Data.inputDataset = '/muons/zichen-muon_step2-da24e452847382f63f18e1483a888c36/USER'
 config.Data.inputDBS = 'phys03'
 
 config.Data.ignoreLocality = True
 config.Data.splitting = 'FileBased'
-config.Data.unitsPerJob = 1
-NJOBS = 100  # This is not a configuration parameter, but an auxiliary variable that we use in the next line.
+config.Data.unitsPerJob = 10
+NJOBS = 10  # This is not a configuration parameter, but an auxiliary variable that we use in the next line.
 config.Data.totalUnits = config.Data.unitsPerJob * NJOBS
 config.Data.outLFNDirBase = '/store/user/%s/' % (getUsernameFromSiteDB())
 config.Data.publication = True
-config.Data.outputDatasetTag = 'muons_step2'
+config.Data.outputDatasetTag = 'muons_step3'
 
 config.Site.whitelist = ["T2_US*"]
 config.Site.storageSite = 'T3_US_FNALLPC'
